eae06b713daa1b8a54d601f1af96ef40445515e4
JODAHAN <dan0748@naver.com>
2023-05-23 04:00:57 +0900

    수정

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,15 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4000",
+      "https://port-0-hwayang-7e6o2clhv5snco.sel4.cloudtype.app",
+    ],
+  })
+);
 
 app.use(
   express.static(
